70636dff65434c3fc62d799b1850fab4690a74e0
Jochen Boesmans <jochen.boesmans@gmail.com>
2019-08-11 22:39:43 +0200

    typo fixes.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SegmentedSieveOfEratosthenes.MixProject do
 
   def project do
     [
-      app: :segmented_sieve_of_eratostenes,
+      app: :segmented_sieve_of_eratosthenes,
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
